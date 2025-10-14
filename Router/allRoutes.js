72832f7d1566fbb5bc06b88dabadc6122051e3b7
cc-didic <cc.cedric.caron@gmail.com>
2025-10-14 08:35:44 +0200

    Ajout des pages d'authentification

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,11 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Acceuil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Changer de mot de passe", "/pages/auth/editPassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
