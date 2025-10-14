f9d29be1a7149b82297a639d138aa9cbea1c7e28
cc-didic <cc.cedric.caron@gmail.com>
2025-10-14 13:47:00 +0200

    Ajout du script connexion

---
+++ js/auth/signin.js
@@ -0,0 +1,27 @@
+const mailInput = document.getElementById("EmailInput");
+const passwordInput = document.getElementById("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
+
+btnSignin.addEventListener("click", checkCredentials);
+
+function checkCredentials(){
+    //Ici, il faudra appeler l'API pour vérifier les credentials en BDD
+
+    //données factif pour le moment
+
+    if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
+        alert("Vous êtes connecté");
+
+        //Il faudra récupérer le vrai token
+        const token = "jghfghghlghkghghkghkghfgdffdfdfd";
+
+        //placer ce token en cookie
+
+        //Retour à la page d'acceuil
+        window.location.replace("/")
+    }
+    else{
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
+    }
+}
